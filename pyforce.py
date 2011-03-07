ea0581a827d7fc09a58b630d0f5b2338ea4d9654
Remigijus Jarmalavičius <remigijus@jarmalavicius.lt>
2011-03-07 22:39:22 +0200

    Added numbers in bruteforce characters list and small improvements in README

--- pyforce.py
+++ pyforce.py
@@ -2,19 +2,19 @@
 # -*- coding: utf-8 -*-
 
 #       pyforce.py
-#       
+#
 #       Copyright 2010 vIiRuS <viirus@pherth.net>
-#       
+#
 #       This program is free software; you can redistribute it and/or modify
 #       it under the terms of the GNU General Public License as published by
 #       the Free Software Foundation; either version 2 of the License, or
 #       (at your option) any later version.
-#       
+#
 #       This program is distributed in the hope that it will be useful,
 #       but WITHOUT ANY WARRANTY; without even the implied warranty of
 #       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #       GNU General Public License for more details.
-#       
+#
 #       You should have received a copy of the GNU General Public License
 #       along with this program; if not, write to the Free Software
 #       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
@@ -23,10 +23,8 @@
 import hashlib
 import sys
 import time
-import thread
 
 def passwordforce(length):
-	print "length", length
 	suggestlist = ["a"]
 	for b in range(1, length):
 		suggestlist.append("a")
@@ -64,5 +62,8 @@ except IndexError:
     print "Specify the hash that you want to bruteforce as first argument"
 else:
 	start = time.clock()
-	characterlist = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+	characterlist = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a",
+                     "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+                     "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w",
+                     "x", "y", "z"]
 	passwordforce(0)
